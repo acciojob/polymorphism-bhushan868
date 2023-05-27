360e468c971dfd769188a3a08445698eb540cc79
bhushan868 <bhushukarale643@gmail.com>
2023-05-27 12:12:45 +0530

    changes made

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -21,7 +21,7 @@ public class Main {
         Product p=new Product();
         System.out.println(p.product(5,10));
         System.out.println(p.product(5,23,5));
-        System.out.println(p.product(5,23,5));
+        System.out.println(p.product(5.0,23.5,5.4));
     }
 }
 
